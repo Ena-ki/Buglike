f8e150085e9af7806ad12a7807c2e60aa9fc7e65
aq1q2q3-stack <aq1q2q3@gmail.com>
2025-09-19 22:12:50 +0900

    Fix: Include remaining merge changes

--- scripts/player/classes/warrior_player_class.gd
+++ scripts/player/classes/warrior_player_class.gd
@@ -3,6 +3,8 @@ extends PlayerClass
 func _ready():
   if movement_ability_scene:
     movement_ability = load_ability(self, movement_ability_scene)
+  if dodge_ability_scene:
+    dodge_ability = load_ability(self, dodge_ability_scene)
   if ability_1_scene:
     ability_1 = load_ability(self, ability_1_scene)
   if ability_2_scene:
